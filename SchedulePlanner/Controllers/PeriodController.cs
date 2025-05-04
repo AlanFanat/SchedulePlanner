79a991ead1267ca8de8b45e5606fd4c74328bcbf
Алан <xalvashokolad@gmail.com>
2025-05-04 10:50:05 +0300

    Добвавил проверку на периоды, надо теперь их создавать

---
+++ SchedulePlanner/Controllers/PeriodController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SchedulePlanner.Db.Models;
+using SchedulePlanner.Db.Repositories;
+using SchedulePlanner.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace SchedulePlanner.Controllers
+{
+    public class PeriodController : Controller
+    {
+        private readonly UserManager<User> userManager;
+        private readonly IPeriodRepository periodRepository;
+
+        public PeriodController(UserManager<User> userManager, IPeriodRepository periodRepository)
+        {
+            this.userManager = userManager;
+            this.periodRepository = periodRepository;
+        }
+        public IActionResult Index()
+        {
+            var userId = Guid.Parse(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var periods = periodRepository.GetByUserId(userId);
+            var periodViewModels = periods.Select(period => PeriodViewModel.FromModel(period)).ToList();
+            return View(periodViewModels);
+        }
+        [HttpPost]
+        public async Task<IActionResult> SelectPeriodAsync(Guid periodId)
+        {
+            var user = await userManager.GetUserAsync(User);
+            user.SelectedPeriodId = periodId;
+            var result = await userManager.UpdateAsync(user);
+            if(result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            return NotFound();
+        }
+    }
+}
---
+++ SchedulePlanner/ViewModels/PeriodViewModel.cs
@@ -0,0 +1,32 @@
+﻿using SchedulePlanner.Db.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SchedulePlanner.ViewModels
+{
+    public class PeriodViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+
+        public string Name { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public static PeriodViewModel FromModel(Period period)
+        {
+            if (period == null)
+                return null;
+
+            return new PeriodViewModel
+            {
+                Id = period.Id,
+                UserId = period.UserId,
+                Name = period.Name,
+                StartDate = period.StartDate,
+                EndDate = period.EndDate
+            };
+        }
+    }
+}
